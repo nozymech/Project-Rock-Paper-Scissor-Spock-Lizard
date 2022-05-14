01410557488dde9a6f5a4c9679952400280f830c
nozymech <sjyang.pt@gmail.com>
2022-05-14 23:05:05 +0800

    display game result

--- script.js
+++ script.js
@@ -4,30 +4,40 @@ function computerPlay() {
 choose = stone[Math.floor(Math.random()*stone.length)]
 return choose
 }
+function playerPlay(){
+  result = "paper"
+return result
+}
 
 function playRound(playerSelection, computerSelection) {
-  if ( playerSelection.toLowerCase() == computerSelection ) {
+  if ( playerSelection== computerSelection ) {
     return "It`s a tie";
-  } else if ( playerSelection.toLowerCase()=="rock" && computerSelection =="Scissors") {
+  } else if ( playerSelection=="rock" && computerSelection =="scissors") {
     return "You win! Rock beats Scissors"
-  } else if (playerSelection.toLowerCase()=="rock" && computerSelection =="paper"){
+  } else if (playerSelection=="rock" && computerSelection =="paper"){
     return "You lose! Paper beats Rocks"
-  } else if ( playerSelection.toLowerCase()=="paper" && computerSelection =="rock") {
+  } else if ( playerSelection=="paper" && computerSelection =="rock") {
     return "You win! Paper beats Rock"
-  }  else if (playerSelection.toLowerCase()=="paper" && computerSelection =="scissors"){
+  }  else if (playerSelection=="paper" && computerSelection =="scissors"){
   return "You lose! Scissor beats Paper"
-  } else if ( playerSelection.toLowerCase()=="scissors" && computerSelection =="paper") {
+  } else if ( playerSelection=="scissors" && computerSelection =="paper") {
     return "You win! Scissor beats Paper"
-  } else if (playerSelection.toLowerCase()=="scissors" && computerSelection =="rock"){
+  } else if (playerSelection=="scissors" && computerSelection =="rock"){
     return "You lose! Rock beats Scissors"
+  }
+  else {
+    return "ERROR"
   }
 }
 
- function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock? Paper? Scissors?");;
+
+function game() {
     const computerSelection = computerPlay();
-    alert(playRound(playerSelection, computerSelection));
+    const playerSelection = playerPlay();
+    const result = document.getElementById("result")
+    result.innerText= playRound(playerSelection, computerSelection);
  }
- }
+
+
+
 
