174a16e6ed8d5adeb1f83170827e3a0781c2cc61
nozymech <sjyang.pt@gmail.com>
2022-05-16 09:37:12 +0800

    Add an event listener to the buttons

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let stone = ["rock","paper","scissors"]
 const result = document.getElementById("result")
 // connect all three buttons to javascript as a node list 
-const buttons = document.querySelectorAll('button');
+const buttons = [].slice.call(document.querySelectorAll('button'));
 const player = document.getElementById("player");
 const computer = document.getElementById("computer")
 let playerScore = 0 ;
 let computerScore = 0 ;
+let buttonClick = 0
 player.innerHTML = `Player: ${playerScore} point  `;
 computer.innerHTML = `Computer: ${computerScore} point`;
 result.innerText= "It shows win or lose"
+  //get index from forEach
+buttons.map((button,index) => {
+  // for each button wii have a event listener
+  button.addEventListener('click',()=>{
+     buttonClick =index;
+  })
+});
 
 function playerAddOne()  {
   playerScore= playerScore + 1;
@@ -21,38 +29,32 @@ function computerAddOne()  {
 }
 
 function computerPlay() {
-  choose = stone[Math.floor(Math.random()*stone.length)]
-  return choose
+  return Math.floor(Math.random()*stone.length)
 }
 function playerChoose() {
-  //map() is to loop through each button
-  buttons.forEach((button) => {
-    // for each button have a event listener
-    button.addEventListener('click',()=>{
-       alert(button.id)
-    })
-  });
+  //map() is to loop through each button (have to form array)
 }
 
 function playRound(playerSelection, computerSelection) {
+  //0: rock 1:paper 2:scissor
   if ( playerSelection== computerSelection ) {
     return "It`s a tie";
-  } else if ( playerSelection=="rock" && computerSelection =="scissors") {
+  } else if ( playerSelection==0 && computerSelection ==2) {
     playerAddOne();
     return "You win! Rock beats Scissors" 
-  } else if (playerSelection=="rock" && computerSelection =="paper"){
+  } else if (playerSelection==0 && computerSelection ==1){
     computerAddOne();
     return "You lose! Paper beats Rocks"
-  } else if ( playerSelection=="paper" && computerSelection =="rock") {
+  } else if ( playerSelection==1 && computerSelection ==0) {
     playerAddOne()
     return "You win! Paper beats Rock"
-  }  else if (playerSelection=="paper" && computerSelection =="scissors"){
+  }  else if (playerSelection==1 && computerSelection ==2){
     computerAddOne()
     return "You lose! Scissor beats Paper"
-  } else if ( playerSelection=="scissors" && computerSelection =="paper") {
+  } else if ( playerSelection==2 && computerSelection ==1) {
     playerAddOne()
     return "You win! Scissor beats Paper"
-  } else if (playerSelection=="scissors" && computerSelection =="rock"){
+  } else if (playerSelection==2 && computerSelection ==0){
     computerAddOne()
     return "You lose! Rock beats Scissors"
   }
@@ -61,8 +63,7 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
   let computerSelection = computerPlay();
-  //let playerSelection= playerChoose();
-  playerSelection= "paper"
+  let playerSelection= buttonClick;
   result.innerText= playRound(playerSelection, computerSelection);
 }
   
